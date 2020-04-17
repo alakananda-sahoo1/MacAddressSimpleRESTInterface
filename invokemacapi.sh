1ecdb505df1fabfd8af9473ef7bb3c2f400804e6
alakananda-sahoo1 <58776777+alakananda-sahoo1@users.noreply.github.com>
2020-04-17 20:17:03 +0530

    Added code line comments and purposes
    Added code line comments and purposes

--- invokemacapi.sh
+++ invokemacapi.sh
@@ -1,6 +1,8 @@
 echo "Calling mac address api"
+# Passing the api uri
 apiurl='https://api.macaddress.io/v1'
 Message='{"apikey": "'"$1"'", "MacAddress": "'"$2"'"}'
+#Will provide output of HTTP response as per below with desired values after hitting into destination server
 HTTP_RESPONSE$(curl --request POST --data "$Message" -H "Content-Type:application/json" "$apiurl")
 HTTP_BODY=$(echo $HTTP_RESPONSE | sed -e 's/HTTPSTATUS\:.*//g')
 echo "Response from api is $HTTP_BODY"
