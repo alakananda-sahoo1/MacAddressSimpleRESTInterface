01b99da0a1e11c5af24c7c2db5e13d4a4b7d90d6
alakananda-sahoo1 <58776777+alakananda-sahoo1@users.noreply.github.com>
2020-04-17 20:15:12 +0530

    Added comment for code line
    Added comment for code line

--- mac_address_identifier.sh
+++ mac_address_identifier.sh
@@ -1,21 +1,24 @@
 echo "apikey is : $1, macaddress: $2"
+# The below line of code will validate null value entry for security authentication of api_key
 if [ $1 == "" ]; then
 	echo "Please enter apikey"
 	exit 1
 fi
-
+#This parameter will check for mac address as entered from user 
 if [ $2 == ""]; then
 	echo "Please enter macaddress"
 	exit 1
 fi
 
 echo "Create docker images"
+# The below is command is creating from current directory 
 docker build -t macaddressimage .
 echo "Images created $macaddressimage"
 
 echo "Create docker container"
+#This parameter is picking unique container id and creating container up and running 
 containerid=$(docker run -dti --name macaddress macaddressimage)
-
+#Based upon container id in case null will exit else will proceed further to log in into docker container
 if [ $containerid == "" ];then
 	echo "Container not created successfully Aborting the execution"
 	exit 1
