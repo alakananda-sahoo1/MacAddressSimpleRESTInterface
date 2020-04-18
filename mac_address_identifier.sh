a3ba9120d33b1557691193dd4faa6f784e2f4e98
alakananda-sahoo1 <58776777+alakananda-sahoo1@users.noreply.github.com>
2020-04-18 22:24:36 +0530

    Added container removal steps
    Added container removal steps

--- mac_address_identifier.sh
+++ mac_address_identifier.sh
@@ -1,28 +1,34 @@
 echo "apikey is : $1, macaddress: $2"
-# The below line of code will validate null value entry for security authentication of api_key
 if [ $1 == "" ]; then
 	echo "Please enter apikey"
 	exit 1
 fi
-#This parameter will check for mac address as entered from user 
+
 if [ $2 == ""]; then
 	echo "Please enter macaddress"
 	exit 1
 fi
 
-echo "Create docker images"
-# The below is command is creating from current directory 
+echo "Creating docker images"
 docker build -t macaddressimage .
 echo "Images created $macaddressimage"
 
-echo "Create docker container"
-#This parameter is picking unique container id and creating container up and running 
-containerid=$(docker run -dti --name macaddress macaddressimage)
-#Based upon container id in case null will exit else will proceed further to log in into docker container
+echo "Creating docker container"
+echo "docker run -dti --name macaddress macaddressimage $1 $2"
+containerid=$(docker run -dti --name macaddress  macaddressimage $1 $2)
+echo "Container id : $containerid"
+
 if [ $containerid == "" ];then
 	echo "Container not created successfully Aborting the execution"
 	exit 1
 else
 	echo "logging into the container"
-	docker exec -it $containerid /bin/ash -c '/home/macaddress/invokemacapi.sh $1 $2'
+	echo "docker exec -it $containerid /bin/ash -c "/home/macaddress/invokemacapi.sh '$1' '$2'""
+	docker exec -it $containerid /bin/ash -c "/home/macaddress/invokemacapi.sh '$1' '$2'"
 fi
+
+echo "stoping and Deleting the container"
+echo "docker stop $containerid"
+docker stop $containerid
+echo "docker rm $containerid"
+docker rm $containerid
