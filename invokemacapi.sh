ffa63d1be61f4f47cb4949e3633fdc5491602a99
alakananda-sahoo1 <58776777+alakananda-sahoo1@users.noreply.github.com>
2020-04-18 22:23:30 +0530

    rectified API URL
    rectified API URL

--- invokemacapi.sh
+++ invokemacapi.sh
@@ -1,8 +1,8 @@
-echo "Calling mac address api"
+echo "apikey: $1 and macaddress: $2"
 # Passing the api uri
-apiurl='https://api.macaddress.io/v1'
-Message='{"apikey": "'"$1"'", "MacAddress": "'"$2"'"}'
+apiurl="https://api.macaddress.io/v1?apiKey=$1&output=json&search=$2"
 #Will provide output of HTTP response as per below with desired values after hitting into destination server
-HTTP_RESPONSE$(curl --request POST --data "$Message" -H "Content-Type:application/json" "$apiurl")
-HTTP_BODY=$(echo $HTTP_RESPONSE | sed -e 's/HTTPSTATUS\:.*//g')
-echo "Response from api is $HTTP_BODY"
+echo "Calling mac address api: $apiurl"
+echo "curl --request GET -H Content-Type:application/json $apiurl"
+HTTP_RESPONSE=$(curl --request GET -H "Content-Type:application/json" "$apiurl" | jq '.vendorDetails.companyName')
+echo "$2 MAC address belongs to $HTTP_RESPONSE"
